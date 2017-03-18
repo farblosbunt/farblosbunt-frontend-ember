50cdb870821988e1e65a3fcffb8b6d21b125fdf3
Andreas Solleder <asol@num42.de>
2017-03-18 02:52:09 +0100

    persist selected tag on index controller when chnaging it in tag/index

--- app/controllers/tag/index.js
+++ app/controllers/tag/index.js
@@ -1,8 +1,12 @@
 import Controller from 'ember-controller';
+import injectController from 'ember-controller/inject';
 
 export default Controller.extend({
+  indexController:  injectController('index'),
+
   actions:{
     onTagSelect(tag){
+      this.get('indexController').set('selected', tag);
       this.transitionToRoute('tag', tag.id);
     }
   }
